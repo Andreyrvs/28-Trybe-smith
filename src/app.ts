fe68c5322f4e955a802f0e6c538bf473351719dc
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-04 20:51:07 -0300

    chore: Req 03 (App) - Adiciona a rota /users OO

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import ProductRoutes from './routes/productRoutes';
+import UserRoutes from './routes/userRoutes';
 
 const app = express();
 
 app.use(express.json());
 console.log('Inicio#');
 app.use('/products', ProductRoutes);
+app.use('/users', UserRoutes);
 
 export default app;
