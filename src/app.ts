1aa8b65b5c175f66a70d6969119245afe09b71bb
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-04 18:22:02 -0300

    chore: Req 01 (App) - Importa o arquivo de rotas e faz a aplicação utilizar

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import ProductRoutes from './routes/productRoutes';
 
 const app = express();
 
 app.use(express.json());
-console.log('Inicio#SS');
+console.log('Inicio#');
+app.use('/products', ProductRoutes);
 
 export default app;
