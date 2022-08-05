a00913b5af821f2e2af6689be74b7e8da0651692
Andrey Rannerson Visniewski <cortaesai@gmail.com>
2022-08-04 21:43:58 -0300

    chore: Req 04 (app) - usa rota /orders

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import ProductRoutes from './routes/productRoutes';
 import UserRoutes from './routes/userRoutes';
+import OrderRoutes from './routes/ordersRouter';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 console.log('Inicio#');
 app.use('/products', ProductRoutes);
 app.use('/users', UserRoutes);
+app.use('/orders', OrderRoutes);
 
 export default app;
