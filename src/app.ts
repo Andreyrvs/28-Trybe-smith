12bbd9ff0b1c87589ecb49fa6fd3d48c94e808ea
Andrey Visniewski <cortaesai@gmail.com>
2023-03-10 18:04:27 -0300

    feature: (API) - Swagger e leitor de YAML

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import YAML from 'yamljs';
 import ProductRoutes from './routes/productRoutes';
 import UserRoutes from './routes/userRoutes';
 import OrderRoutes from './routes/ordersRouter';
 
-const app = express();
+const swaggerDocument = YAML.load('./swagger.yaml');
 
+const app = express();
 app.use(express.json());
+
+app.use('/', swaggerUi.serve);
+app.get('/', swaggerUi.setup(swaggerDocument));
+
 console.log('Inicio#');
 app.use('/products', ProductRoutes);
 app.use('/users', UserRoutes);
